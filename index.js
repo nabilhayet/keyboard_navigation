a870be0ce168f2ea3b0697fe5133f6c2ee310c26
nabilhayet <nabil4457@yahoo.com>
2024-08-23 17:21:48 -0400

    added listening text as voice and recording speech

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+if ('speechSynthesis' in window) {
+    alert("Broswer supports speech synthesis 🎉");
+} else {
+    alert("Sorry, your browser doesn't support the speech synthesis API !");
+}
+
+document.getElementById('b').onkeypress = clickMeHandler;
+
+function clickMeHandler(e) {
+    // console.log("click me handler, type: ", e.type);
+    // console.log("click me handler, target: ", e.target);
+
+    let pTag = document.getElementsByTagName("p")[0].innerHTML
+    let text = pTag
+
+    // Create a new SpeechSynthesisUtterance object
+    let utterance = new SpeechSynthesisUtterance();
+
+    // Set the text and voice of the utterance
+    utterance.text = text;
+    utterance.voice = window.speechSynthesis.getVoices()[0];
+
+    // Speak the utterance
+    window.speechSynthesis.speak(utterance);
+}
+
+
 const startButton = document.getElementById('startButton');
 const outputDiv = document.getElementById('output');
 
